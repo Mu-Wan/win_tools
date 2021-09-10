3b427e8e7f04f39d1e8140ca74a8bc11668f6204
皱眉 <272050771@qq.com>
2021-09-10 09:54:35 +0800

    新增壁纸尺寸(=1080/1920.1440/2560)

--- select_pic.py
+++ select_pic.py
@@ -4,11 +4,12 @@ from PIL import Image
 
 appro_per = 1.2  # 近方的宽高比
 base_path = os.getcwd()
-pic_path = [base_path + '\\横', base_path + '\\竖', base_path + '\\方', base_path + '\\近方']
+pic_path = [base_path + '\\横', base_path + '\\竖', base_path + '\\方', base_path + '\\近方', base_path + '\\壁纸']
 pic_sec_path = [pic_path[0] + '\\1080', pic_path[0] + '\\2k', pic_path[0] + '\\none',
                 pic_path[1] + '\\1080', pic_path[1] + '\\2k', pic_path[1] + '\\none',
                 pic_path[2] + '\\1080', pic_path[2] + '\\2k', pic_path[2] + '\\none',
-                pic_path[3] + '\\1080', pic_path[3] + '\\2k', pic_path[3] + '\\none']
+                pic_path[3] + '\\1080', pic_path[3] + '\\2k', pic_path[3] + '\\none',
+                pic_path[4] + '\\1080', pic_path[4] + '\\2k']
 
 for path in pic_path:
     if not os.path.exists(path):
@@ -44,10 +45,15 @@ appro_sq_2k = [i for i in range(len(size_list))
 appro_sq_none = [i for i in range(len(size_list))
                  if height_list[i] < width_list[i] <= height_list[i] * appro_per < 1080
                  or width_list[i] < height_list[i] <= width_list[i] * appro_per < 1080]
+# 壁纸
+wall_1080 = [i for i in range(len(size_list)) if height_list[i] == 1080 and width_list[i] == 1920]
+wall_2k = [i for i in range(len(size_list)) if height_list[i] == 1440 and width_list[i] == 2560]
+# 操作
 pic_list = [hor_1080, hor_2k, hor_none,
             ver_1080, ver_2k, ver_none,
             square_1080, square_2k, square_none,
-            appro_sq_1080, appro_sq_2k, appro_sq_none]
+            appro_sq_1080, appro_sq_2k, appro_sq_none,
+            wall_1080, wall_2k]
 for i in range(len(pic_list)):
     for j in pic_list[i]:
         shutil.copy(file_list[j], pic_sec_path[i])
